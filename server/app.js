5caff5f658a74cb08a5bf7b7cc8b0e9035bd1fd6
Ahmed Fawzy <fawzy@711media.tv>
2020-06-24 01:45:51 -0700

    Adding Cors - try catch blocks - Netlify redirects file

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require("cors");
 
 const mongoose = require("mongoose");
 const mongoURI = process.env.MONGODB_URI
@@ -14,7 +15,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
--- server/app/modules/users/UsersController.js
+++ server/app/modules/users/UsersController.js
@@ -6,21 +6,24 @@ UsersController.getUsers = (req, res) => {
   res.json({ status: "success", message: "Welcome To Users API" });
 };
 
-UsersController.addUser = async (req, res) => {
+UsersController.addUser = async (req, res, next) => {
   // const data = req.body;
+  try {
+    const user = new User({
+      name: "fady",
+      age: "26",
+      phone: "3237125693",
+    });
 
-  const user = new User({
-    name: "fady",
-    age: "26",
-    phone: "3237125693",
-  });
-
-  const insertedUser = await user.save();
-  res.json({
-    status: "success",
-    message: "User added successfully!",
-    user: insertedUser,
-  });
+    const insertedUser = await user.save();
+    res.json({
+      status: "success",
+      message: "User added successfully!",
+      user: insertedUser,
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = UsersController;
